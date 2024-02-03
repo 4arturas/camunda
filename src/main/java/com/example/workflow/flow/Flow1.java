8a35f97eb1b280c334912eda564dd5ef1458ba2c
Your Name <you@example.com>
2024-02-03 08:44:54 +0200

    2024-02-03

--- src/main/java/com/example/workflow/flow/Flow1.java
+++ src/main/java/com/example/workflow/flow/Flow1.java
@@ -1,10 +1,11 @@
 package com.example.workflow.flow;
 
-import camundajar.impl.scala.Int;
 import jakarta.inject.Named;
 import lombok.extern.slf4j.Slf4j;
+import netscape.javascript.JSObject;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.spin.impl.json.jackson.JacksonJsonNode;
 
 @Slf4j
 @Named(value="Flow1")
@@ -16,6 +17,7 @@ public class Flow1 implements JavaDelegate
         String creditor = (String) delegateExecution.getVariable("creditor");
         Double amount = (Double) delegateExecution.getVariable("amount");
         Integer counter = (Integer) delegateExecution.getVariable("counter");
+        JacksonJsonNode broker = (JacksonJsonNode) delegateExecution.getVariable("broker");
         final Integer random = (Integer) delegateExecution
                 .getVariable(
                         com.example.workflow.flow.Flow2.RANDOM
